caa1c6bb25c4adc1ffdedadfd244e4a4fe310efa
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-29 15:52:36 +0200

    aggiunto componente MyHeader

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script>
+import MyHeader from './components/MyHeader.vue';
+export default {
+  components: {
+    MyHeader,
+  }
+}
 </script>
 
 <template>
-  <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-      </p>
-    </div>
-  </div>
+  <MyHeader></MyHeader>
+
+  <main>
+
+  </main>
 </template>
 
 <style lang="scss">
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'MyHeader',
+}
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-center">Yuo-Ghi oh</h1>
+
+
+    </div>
+</template>
